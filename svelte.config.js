2b792c2f9543cb630b1105c82e35977506a6d3c1
CROpie <96667010+CROpie@users.noreply.github.com>
2023-06-18 15:59:33 +1000

    changed from netlify to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
-/*
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
- @type {import('@sveltejs/kit').Config}
+/* @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
@@ -12,21 +11,3 @@ const config = {
 };
 
 export default config;
-*/
-import adapter from '@sveltejs/adapter-netlify';
-
-export default {
-	kit: {
-		// default options are shown
-		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			edge: false,
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false
-		})
-	}
-};
